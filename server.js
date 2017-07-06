0f32fa50efbb994f6da60dbd9cbb613032d5216c
suhas <suhas.m@bvbi-infotech.com>
2017-07-06 12:19:21 +0530

    Updated copy

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ var app = express();
 app.use(express.static('public'));
 
 app.listen(3000, function () {
-  console.log('Express server is up on port 3000');
+  console.log('App is up on port 3000');
 });
