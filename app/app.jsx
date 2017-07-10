5442c391918964c19d591deae62f4bf10037c728
suhas <suhas.m@bvbi-infotech.com>
2017-07-10 14:40:38 +0530

    Included Foundation and Jquery

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,11 @@ var Main=require('Main');
 var Weather=require('Weather');
 var About=require('About');
 var Examples=require('Examples');
+
+//Load Foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 ReactDOM.render(
     <Router history={hashHistory}>
         <Route path="/" component={Main}>
