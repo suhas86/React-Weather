72db03312dc611645ede00ea5fcb6ac98eeae5d3
suhas <suhas.m@bvbi-infotech.com>
2017-07-10 15:33:31 +0530

    About us Fix

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,9 +9,32 @@ var React=require('react');
 })*/
 
 var About=(props)=>{
-    return(
-        <h3>About Component</h3>
-    );
+    return  (
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application build on React. I have built this
+        for practising ReactJS.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the
+            JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used
+            Open Weather Map to search for weather data by city name.
+        </li>
+        <li>
+          <a href="https://github.com/suhas86/React-Weather">Git Url</a> - You can find the
+          updated code on my GIT repository.
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 module.exports=About;
